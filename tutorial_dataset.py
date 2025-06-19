08322c6ece7aea6ad866fffcb73deb740afa0f19
Yozi <66948150+YossiSella@users.noreply.github.com>
2025-06-19 14:29:38 +0300

    Modified the tutorial to preprocess the fill50k dataset on my machine - added function to read the images and changed paths

--- tutorial_dataset.py
+++ tutorial_dataset.py
@@ -4,11 +4,20 @@ import numpy as np
 
 from torch.utils.data import Dataset
 
+# Function to read images with unicode paths
+# This is necessary because OpenCV does not handle unicode paths directly on Windows.
+def imread_unicode(path):
+    # Read file manually and decode as image
+    stream = open(path, "rb")
+    bytes_array = bytearray(stream.read())
+    np_array = np.asarray(bytes_array, dtype=np.uint8)
+    img = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
+    return img
 
 class MyDataset(Dataset):
     def __init__(self):
         self.data = []
-        with open('./training/fill50k/prompt.json', 'rt') as f:
+        with open(r"C:\Users\yossi\Documents\Bar Ilan\Master's\תשפה - סמסטר א\מודלים גנרטיביים עמוקים\Final_project\NSTTS\datasets\fill50k\fill50k\prompt.json", 'rt') as f:
             for line in f:
                 self.data.append(json.loads(line))
 
@@ -22,9 +31,12 @@ class MyDataset(Dataset):
         target_filename = item['target']
         prompt = item['prompt']
 
-        source = cv2.imread('./training/fill50k/' + source_filename)
-        target = cv2.imread('./training/fill50k/' + target_filename)
+        source = imread_unicode(r"C:\Users\yossi\Documents\Bar Ilan\Master's\תשפה - סמסטר א\מודלים גנרטיביים עמוקים\Final_project\NSTTS\datasets\fill50k\fill50k\\" + source_filename)
+        target = imread_unicode(r"C:\Users\yossi\Documents\Bar Ilan\Master's\תשפה - סמסטר א\מודלים גנרטיביים עמוקים\Final_project\NSTTS\datasets\fill50k\fill50k\\" + target_filename)
 
+        if source is None or target is None or prompt is None:
+            raise ValueError(f"Missing data for index {idx}: source={source_filename}, target={target_filename}, prompt={prompt}")
+        
         # Do not forget that OpenCV read images in BGR order.
         source = cv2.cvtColor(source, cv2.COLOR_BGR2RGB)
         target = cv2.cvtColor(target, cv2.COLOR_BGR2RGB)
